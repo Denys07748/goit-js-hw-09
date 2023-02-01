4bb112d936778bb629f2c24dd0c2d5b6fa0bef2d
Denys07748 <95140883+Denys07748@users.noreply.github.com>
2023-02-01 20:16:57 +0000

    Deploying to gh-pages from @ Denys07748/goit-js-hw-09@58c9fc9dcca3d7b47b5bd19d981a2bb26a5ae0a5 🚀

--- 01-color-switcher.0d710304.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let n=null;t.addEventListener("click",(function(){n=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.disabled=!0})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(n)}));
-//# sourceMappingURL=01-color-switcher.0d710304.js.map
