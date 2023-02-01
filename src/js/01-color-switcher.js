fe5a9dbbd1a9bf3b604e6be95bc9bafd1936e480
Denys07748 <denis07748@gmail.com>
2023-02-01 21:35:44 +0200

    додає task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const btnStartEl = document.querySelector('[data-start]');
+const btnStopEl = document.querySelector('[data-stop]');
+let timerId = null;
 
+btnStartEl.addEventListener('click', onStartChangeBgColor);
+btnStopEl.addEventListener('click', onStopChangeBgColor);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onStartChangeBgColor() {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  btnStartEl.disabled = true;
+}
+
+function onStopChangeBgColor() {
+  btnStartEl.disabled = false;
+  clearInterval(timerId);
+}
